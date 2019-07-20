ec53a0123bcf23eee985dcefd69b2fe4cdf78fff
stars_kim <stars_kim@163.com>
2019-07-21 00:10:07 +0800

    更新AJAX处理JSON用户管理

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,14 @@ Route::group(['prefix' => 'auth'], function () {
     Auth::routes();
 });
 //管理
-Route::group(['middleware' => 'auth', 'prefix' => 'admin'], function () {
+Route::group(['prefix' => 'admin'], function () {
     Route::view('/', 'admin.index');
 
     Route::any('/options', 'AdminController@options');
 
-    Route::view('/users-v1', 'admin.users-v1');
-    Route::view('/users-v2', 'admin.users-v2');
+    Route::view('/users', 'admin.users');
 
     Route::any('/user-list', 'AdminController@getUserList');
-    Route::any('/user-lists', 'AdminController@getUserLists');
 });
 //用户
 Route::group(['middleware' => 'auth', 'prefix' => 'user'], function () {
