8286b4836592f3eadeae9bc5637b2e0a9b432ed6
stars_kim <stars_kim@163.com>
2019-07-22 18:01:43 +0800

    权限系统 限制非管理员进入后台

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Blade;
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::if('admin', function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
